59400982af7ca5c4563c36183482b7e7cf4de99b
clavierpaul <53355311+clavierpaul@users.noreply.github.com>
2021-07-15 15:02:55 +0100

    Reduce bundle size

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import svgr from "vite-plugin-svgr";
 import reactJsx from "vite-react-jsx";
+import brotli from "rollup-plugin-brotli";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,22 @@ export default defineConfig({
   build: {
     outDir: "build",
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("three")) {
+            return "three";
+          } else if (id.includes("react")) {
+            return "react";
+          } else if (id.includes("node_modules")) {
+            return "vendor";
+          } else {
+            return "index";
+          }
+        }
+      },
+      plugins: [brotli()]
+    }
   },
   plugins: [reactRefresh(), svgr(), reactJsx()],
 });
