ae9a6428295f795217f185383048c1e178c3e375
clavierpaul <53355311+clavierpaul@users.noreply.github.com>
2021-07-15 15:02:27 +0100

    Disable sentry in development

--- Client/src/index.tsx
+++ Client/src/index.tsx
@@ -4,17 +4,21 @@ import * as Sentry from "@sentry/react";
 import { Integrations } from "@sentry/tracing";
 import "./index.css";
 import App from "./App";
+import process from "process";
 
-Sentry.init({
-  dsn: "https://296ccbcd5ad844a3a781ac6c9ea07c47@o920715.ingest.sentry.io/5866570",
-  integrations: [new Integrations.BrowserTracing()],
-  tunnel: "https://localhost:5001/tunnel",
+// Don't use sentry in development
+if (process.env.NODE_ENV !== "development") {
+  Sentry.init({
+    dsn: "https://296ccbcd5ad844a3a781ac6c9ea07c47@o920715.ingest.sentry.io/5866570",
+    integrations: [new Integrations.BrowserTracing()],
+    tunnel: "https://localhost:5001/tunnel",
 
-  // Set tracesSampleRate to 1.0 to capture 100%
-  // of transactions for performance monitoring.
-  // We recommend adjusting this value in production
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
